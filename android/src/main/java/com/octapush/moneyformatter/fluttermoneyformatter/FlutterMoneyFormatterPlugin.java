ef3fa1c952813072f1c5dd6332f323559a69d844
nabeel.iriq.g <nabeel.iriq.g@gmail.com>
2022-04-18 16:53:22 +0300

    flutter Android embedding v2

--- android/src/main/java/com/octapush/moneyformatter/fluttermoneyformatter/FlutterMoneyFormatterPlugin.java
+++ android/src/main/java/com/octapush/moneyformatter/fluttermoneyformatter/FlutterMoneyFormatterPlugin.java
@@ -5,9 +5,11 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import androidx.annotation.NonNull;
 
 /** FlutterMoneyFormatterPlugin */
-public class FlutterMoneyFormatterPlugin implements MethodCallHandler {
+public class FlutterMoneyFormatterPlugin implements MethodCallHandler, FlutterPlugin {
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_money_formatter");
@@ -21,5 +23,17 @@ public class FlutterMoneyFormatterPlugin implements MethodCallHandler {
     } else {
       result.notImplemented();
     }
+  }
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_money_formatter");
+    channel.setMethodCallHandler(new FlutterMoneyFormatterPlugin());
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+    channel = null;
   }
 }
