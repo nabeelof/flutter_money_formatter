f36e3aa595bab4435ff0a67bf5f68fd1043edead
nabeel.iriq.g <nabeel.iriq.g@gmail.com>
2022-04-18 17:05:24 +0300

    Revert "flutter Android embedding v2" no need for this commit
    This reverts commit ef3fa1c952813072f1c5dd6332f323559a69d844.

--- android/src/main/java/com/octapush/moneyformatter/fluttermoneyformatter/FlutterMoneyFormatterPlugin.java
+++ android/src/main/java/com/octapush/moneyformatter/fluttermoneyformatter/FlutterMoneyFormatterPlugin.java
@@ -5,11 +5,9 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
-import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import androidx.annotation.NonNull;
 
 /** FlutterMoneyFormatterPlugin */
-public class FlutterMoneyFormatterPlugin implements MethodCallHandler, FlutterPlugin {
+public class FlutterMoneyFormatterPlugin implements MethodCallHandler {
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_money_formatter");
@@ -23,17 +21,5 @@ public class FlutterMoneyFormatterPlugin implements MethodCallHandler, FlutterPl
     } else {
       result.notImplemented();
     }
-  }
-
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_money_formatter");
-    channel.setMethodCallHandler(new FlutterMoneyFormatterPlugin());
-  }
-
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-    channel = null;
   }
 }
